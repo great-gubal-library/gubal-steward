d442630000eef9a97e14e8297c710b997eae866c
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2021-02-18 14:38:29 +0200

    [MOD] added command handler

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+const fs  = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
-const TOKEN = process.env.TOKEN;
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
-bot.login(TOKEN);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  
+  bot.commands.set(command.name, command);
+}
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+bot.once("ready", () => {
+  console.log("Ready!");
 });
+
+bot.on("message", message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  if (!bot.commands.has(commandName)) return;
+
+  const command = bot.commands.get(commandName);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
+
+bot.login(token);
