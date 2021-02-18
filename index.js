6bea41c1af10841a2e0d39f32373a9202d53250c
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2021-02-18 15:29:10 +0200

    [MOD] fixed indentations

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs  = require("fs");
+const fs = require("fs");
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 
@@ -8,31 +8,31 @@ bot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  
-  bot.commands.set(command.name, command);
+	const command = require(`./commands/${file}`);
+
+	bot.commands.set(command.name, command);
 }
 
 bot.once("ready", () => {
-  console.log("Ready!");
+	console.log("Ready!");
 });
 
 bot.on("message", message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-  if (!bot.commands.has(commandName)) return;
+	if (!bot.commands.has(commandName)) return;
 
-  const command = bot.commands.get(commandName);
+	const command = bot.commands.get(commandName);
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply("there was an error trying to execute that command!");
-  }
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply("there was an error trying to execute that command!");
+	}
 });
 
 bot.login(token);
